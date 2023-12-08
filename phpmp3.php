e450f9e4168200be22565aab6f21b435a9221b62
minini <125327866+dananopop@users.noreply.github.com>
2023-12-08 06:13:37 +0100

    Update phpmp3.php

--- phpmp3.php
+++ phpmp3.php
@@ -44,7 +44,6 @@ $number = 5;
         }
 <?php
 $token = 'YOUR_BOT_TOKEN';
-$chat_id = 'TARGET_CHAT_ID';
 $message_id = 'MESSAGE_ID';
 
 file_get_contents("https://api.telegram.org/bot$token/editMessageText?chat_id=$chat_id&message_id=$message_id&text=$new_message");
